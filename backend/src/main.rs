6abbc7552a212a27d6ae722a4787927c072c2e5b
Florian Chlan <florian@chlan.net>
2018-04-02 15:38:51 +0200

    Added first example of rust3-ethereum integration

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,11 @@
+extern crate web3;
+
+use web3::futures::Future;
+
 fn main() {
-    println!("Hello, world!");
+  let (_eloop, transport) = web3::transports::Http::new("http://localhost:8545").unwrap();
+  let web3 = web3::Web3::new(transport);
+  let accounts = web3.eth().accounts().wait().unwrap();
+
+  println!("Accounts: {:?}", accounts);
 }

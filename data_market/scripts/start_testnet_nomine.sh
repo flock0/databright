4b2bd989a2c19e71cd9c9628bb1dc677c3bd549e
Florian Chlan <florian@chlan.net>
2018-04-06 17:17:26 +0200

    Added helper scripts for startup

---
+++ data_market/scripts/start_testnet_nomine.sh
@@ -0,0 +1,6 @@
+cd ..
+remixd -s . &
+ipfs daemon &
+geth --rpc --rpcport "8545" --datadir pnet/ --port "30303" --ws --wsport "8546" --wsaddr "127.0.0.1" --rpcapi "personal,db,eth,net,web3" --wsapi "personal,db,eth,net,web3" --wsorigins "*" --unlock 0 --password <(echo "") &
+sleep 10
+mist --rpc ./pnet/geth.ipc &

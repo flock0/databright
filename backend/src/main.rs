84437d3a8ad4839d3e4f6eeed1330fbc0b4195e1
Florian Chlan <florian@chlan.net>
2018-04-13 09:26:38 +0200

    rust-web3: Test of polling of events (work in progress)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,22 +1,34 @@
 extern crate web3;
 extern crate ipfsapi;
+extern crate tokio_core;
 use web3::contract::{Contract, Options};
 use web3::types::{Address, FilterBuilder, BlockNumber};
 use web3::futures::{Future, Stream};
 use ipfsapi::IpfsApi;
+use std::time::Duration;
 
 fn main() {
+
+	let mut event_loop = tokio_core::reactor::Core::new().unwrap();
+    let handle = event_loop.handle();
 	// WEB3 WEBSOCKET
-    let (_eloop, transp) = web3::transports::WebSocket::new("ws://127.0.0.1:8546").unwrap();
+    let transp = web3::transports::WebSocket::with_event_loop("ws://127.0.0.1:8546", &handle).unwrap();
+
     let web3 = web3::Web3::new(transp);
     println!("a");
-    let accounts = web3.eth().accounts().wait().unwrap();
-	println!("b");
-    let my_acc = accounts[0];
-    let balance = web3.eth().balance(my_acc, None).wait().unwrap();
-    println!("Balance of {}: {}", my_acc, balance);
+    let accounts = web3.eth().accounts().map(|accounts| {
+        println!("Accounts: {:?}", accounts);
+        accounts[0]
+	});
+	
+	let my_acc = event_loop.run(accounts).unwrap();
+	let bal = web3.eth().balance(my_acc, None).map(|balance| {
+		println!("Balance of {}: {}", my_acc, balance);
+	});	
+	event_loop.run(bal).unwrap();
+    //println!("Balance of {}: {}", my_acc, balance);
     // Accessing existing contract
-    let contract_address: Address = "18c06846a71256d3af75f4b2948ba475e714b45b".parse().unwrap(); // Address of the deployed SimpleDatabase contract
+    let contract_address: Address = "504aeef21184dc59f01be167264c60bfa8560699".parse().unwrap(); // Address of the deployed SimpleDatabase contract
     let contract = Contract::from_json(
         web3.eth(),
         contract_address,
@@ -31,20 +43,46 @@ fn main() {
 
 	println!("{}", data);
 
+	/*
 	// SEND TRANSACTION
     let mut options = Options::default();
     options.gas = Some(200000.into());
     let result = contract.call("addShard", (my_acc, "test".to_string(),), my_acc, options);
     let unwrapped_res = result.wait().unwrap();
     println!("{}", unwrapped_res);
+    let mut options = Options::default();
+    options.gas = Some(200000.into());
+    let result = contract.call("addShard", (my_acc, "test2".to_string(),), my_acc, options);
+    let unwrapped_res = result.wait().unwrap();
+    println!("{}", unwrapped_res);
+    */
+
+    // FILTER PAST LOGS
+    println!("c");
+    //let filt = FilterBuilder::default().address(vec![contract_address]).limit(10).build();
+    // .from_block(BlockNumber::Earliest).to_block(BlockNumber::Latest).
+    let filt = FilterBuilder::default().build();
+    println!("d");
+    let logfilter = web3.eth_filter().create_logs_filter(filt);
+    let filter_stream = event_loop.run(logfilter).unwrap();
+    println!("e");
+    let logg = filter_stream.poll();
+
+    /*
+    let print_logs = filter_stream
+    	.for_each(|x| {
+            println!("Got: {:?}", x);
+            Ok(())
+        });
+    println!("f");
+    */
+    let returnval = event_loop.run(logg).unwrap();
+    println!("{:?}", returnval);
     
-    // FILTER LOGS
-    let filt = FilterBuilder::default().from_block(BlockNumber::Earliest).to_block(BlockNumber::Latest).address(vec![contract_address]).build();
-    let mut sub = web3.eth_subscribe().subscribe_logs(filt).wait().unwrap();
+    /*let mut sub = web3.eth_subscribe().subscribe_logs(filt).wait().unwrap();
 	println!("Got subscription id: {:?}", sub.id());
 
 	(&mut sub)
-        .take(5)
         .for_each(|x| {
             println!("Got: {:?}", x);
             Ok(())
@@ -53,4 +91,5 @@ fn main() {
         .unwrap();
 
 	sub.unsubscribe();
+	*/
 }
